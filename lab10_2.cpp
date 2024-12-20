1e6252bc5b2b63e157565a46d402068b7ac232e4
Suphavej <fatkimmmm@gmail.com>
2024-12-20 13:11:26 +0700

    finished

--- lab10_2.cpp
+++ lab10_2.cpp
@@ -3,16 +3,36 @@
 #include<string>
 using namespace std;
 
-int main (){
-	ifstream source;
-	ofstream dest;
-	source.open("cheerbook.txt") ;
-	dest.open("cheerbook_copy.txt");
-	
-	//กรอก Code เฉพาะส่วนนี้ ให้สอดคล้องกับโจทย์และ Code ส่วนอื่น ๆ 
-	//โดยห้ามแก้ไข Code ในส่วนอื่น ๆ
+int main() {
+    ifstream source;
+    ofstream dest;
+    source.open("cheerbook.txt");
+    dest.open("cheerbook_copy.txt");
+
+    if (!source) {
+        cerr << "Error: Unable to open cheerbook.txt for reading." << endl;
+        return 1;
+    }
+
+    if (!dest) {
+        cerr << "Error: Unable to create cheerbook_copy.txt for writing." << endl;
+        return 1;
+    }
+
+    
+    dest << "-------------------- BOOM ---------------------" << endl;
+
+    
+    string line;
+    while (getline(source, line)) {
+        dest << line << endl;
+    }
+
+    
+    dest << "-------------------- HA!! ---------------------" << endl;
 
     source.close();
     dest.close();
-	return 0;
+
+    return 0;
 }
